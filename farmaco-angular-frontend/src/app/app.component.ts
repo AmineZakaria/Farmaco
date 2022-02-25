de548e788ab0b981bf939fc2235a2880864b34bd
AmineZakaria99 <amine.zakaria@supcom.tn>
2022-02-25 15:55:51 +0100

    message3

--- farmaco-angular-frontend/src/app/app.component.ts
+++ farmaco-angular-frontend/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FARMACO';
+  ngOnInit() {
+    (function(d, m){
+        var kommunicateSettings = {"appId":"26bb66afd24526f7cde98acca84119f2b","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+  }
 }
